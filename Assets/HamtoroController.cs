1bca00003fe4555ccee7d91192c03b4d31dc7780
janezhanggg <jane.zhang@wustl.edu>
2025-02-08 01:52:21 -0600

    added sprites + obstacles

--- Assets/HamtoroController.cs
+++ Assets/HamtoroController.cs
@@ -14,27 +14,27 @@ public class HamtoroController : MonoBehaviour
         // move up
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += new Vector3(0, 0.2f, 0);
+            transform.position += new Vector3(0, 0.05f, 0);
         }
 
         // move down
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += new Vector3(0, -0.2f, 0);
+            transform.position += new Vector3(0, -0.05f, 0);
         }
 
 
         // move left
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += new Vector3(-0.2f, 0, 0);
+            transform.position += new Vector3(-0.05f, 0, 0);
         }
 
 
         // move right
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += new Vector3(0.2f, 0, 0);
+            transform.position += new Vector3(0.05f, 0, 0);
         }
     }
 }
