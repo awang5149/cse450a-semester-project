dda2b6e312be37ac541ddb7686e0c3b24b2a6b95
jamies31 <89811255+jamies31@users.noreply.github.com>
2025-02-13 10:38:55 -0600

    Fixed jumping movement for hamtaro.  Added eagle, seed, ground blocks as prefabs for reuse Started creating level 1 setting (obstacles)

---
+++ Assets/Code/HamtoroController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HamtoroController : MonoBehaviour
+{
+    // Outlet
+    Rigidbody2D _rigidbody2D;
+    // public Transform aimPivot;
+    // public GameObject projectilePrefab;
+    
+    // Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        // Aim toward mouse
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector3 directionFromPlayerToMouse = mousePositionInWorld - transform.position;
+
+        float radiansToMouse = Mathf.Atan2(directionFromPlayerToMouse.y, directionFromPlayerToMouse.x);
+        float angleToMouse = radiansToMouse * Mathf.Rad2Deg;
+
+        aimPivot.rotation = Quaternion.Euler(0, 0, angleToMouse);
+
+        // Shoot
+        if(Input.GetMouseButtonDown(0)){
+            GameObject newProjectile = Instantiate(projectilePrefab);
+            newProjectile.transform.position = transform.position;
+            newProjectile.transform.rotation = aimPivot.rotation;
+        }
+        */
+
+        // Jump
+        if(Input.GetKeyDown(KeyCode.Space)){
+            _rigidbody2D.AddForce(Vector2.up * 15f, ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D other){
+        // Check that we collided w ground
+        if(other.gameObject.layer == LayerMask.NameToLayer("Ground")){
+            RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.down, 0.7f);
+
+            for(int i=0; i<hits.Length; i++){
+                RaycastHit2D hit = hits[i];
+            }
+        }
+    }
+
+    /*
+    // this code should reset scene when hamtaro hits an obstacle but not working rn
+    void onTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.GetComponent<HamtoroController>()){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    */
+}
