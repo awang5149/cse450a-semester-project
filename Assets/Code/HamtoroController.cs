4effbbfa7ec46324557b1b33850af8ca5d25d5d9
William Yin <w.m.yin@wustl.edu>
2025-02-27 17:42:30 -0600

    Made Level 2

--- Assets/Code/HamtoroController.cs
+++ Assets/Code/HamtoroController.cs
@@ -5,16 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class HamtoroController : MonoBehaviour
 {
-    // Outlet
     Rigidbody2D _rigidbody2D;
-    // public Transform aimPivot;
-    // public GameObject projectilePrefab;
+    public float moveSpeed = 5f;
+    private bool isGrounded = false;
 
-    // Methods
 
-    // State Tracking
     public int jumpsLeft;
-    // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
@@ -23,24 +19,13 @@ public class HamtoroController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
-        // Aim toward mouse
-        Vector3 mousePosition = Input.mousePosition;
-        Vector3 mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePosition);
-        Vector3 directionFromPlayerToMouse = mousePositionInWorld - transform.position;
 
-        float radiansToMouse = Mathf.Atan2(directionFromPlayerToMouse.y, directionFromPlayerToMouse.x);
-        float angleToMouse = radiansToMouse * Mathf.Rad2Deg;
-
-        aimPivot.rotation = Quaternion.Euler(0, 0, angleToMouse);
-
-        // Shoot
-        if(Input.GetMouseButtonDown(0)){
-            GameObject newProjectile = Instantiate(projectilePrefab);
-            newProjectile.transform.position = transform.position;
-            newProjectile.transform.rotation = aimPivot.rotation;
+        // Horizontal Movement
+        float moveInput = Input.GetAxis("Horizontal");
+        if (isGrounded)
+        {
+            _rigidbody2D.velocity = new Vector2(moveInput * moveSpeed, _rigidbody2D.velocity.y);
         }
-        */
         // Jump
         if (Input.GetKeyDown(KeyCode.Space)){
             if (jumpsLeft > 0)
@@ -64,6 +49,23 @@ public class HamtoroController : MonoBehaviour
                     jumpsLeft = 2;
                 }
             }
+        }
+    }
+
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 
